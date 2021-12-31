7381b57722119455b322c693922e482fa55d0cf7
caH40 <kmvsancho@gmail.com>
2021-12-31 16:08:02 +0300

    add weekfilter

--- botreadlogs.js
+++ botreadlogs.js
@@ -34,7 +34,7 @@ bot.start(async ctx => {
 })
 bot.command('logs', async ctx => {
 	if (admins.includes(ctx.message.from.username)) {
-		await ctx.reply('chose the day', {
+		await ctx.reply('chose the TelegramBot', {
 			reply_markup: {
 				inline_keyboard: [
 					[
@@ -47,13 +47,14 @@ bot.command('logs', async ctx => {
 		await ctx.reply('У вас нет прав пользоваться сервисом, срочно покинте данный чат!')
 	}
 })
-const milliSecondsInWeek = 7 * 24 * 60 * 60 * 1000
+const secondsInWeek = 7 * 24 * 60 * 60
 bot.on('callback_query', async ctx => {
 	// console.log(ctx.update.callback_query.data)
 	if (ctx.update.callback_query.data === 'BotCurrant') {
 		async function request() {
 			try {
-				const logMessage = await Message.find({ $and: [{ "message.text": /^\/\w+/i }, { "message.date": { $gte: new Date().getTime() - milliSecondsInWeek } }] })
+				const logMessage = await Message.find({ $and: [{ "message.text": /^\/\w+/i }, { "message.date": { $gte: (new Date().getTime()) / 1000 - secondsInWeek } }] })
+				// const logMessage = await Message.find({ $and: [{ "message.text": /^\/\w+/i }, { "message.date": { $gte: new Date().getTime() - milliSecondsInWeek } }] })
 				for (let i = 0; i < logMessage.length; i++) {
 					await ctx.reply(`${new Date(logMessage[i].message.date * 1000).toLocaleString()}, ${logMessage[i].message.chat.title ??= 'private'}, ${logMessage[i].message.from.username ??= 'Незнакомец'}, ${logMessage[i].message.text}`)
 				}
